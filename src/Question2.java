4083a1fc2e615bc22ce65393d8126b7666c7633c
Pedro Cunha <phcs-12@hotmail.com>
2022-02-21 01:59:46 -0300

    Removendo print de teste

--- src/Question2.java
+++ src/Question2.java
@@ -52,7 +52,6 @@ public class Question2 {
                 }
             }
         }
-        System.out.println(count);
         count = 4 - count;
         if(input.length() + count < 6){
             count +=  6 - (input.length() + count);
